e6ab5987692b6eea3791e1c0bc374f8299e1de4f
Gabry-Fara <gabriele.faraone.gf@gmail.com>
2025-05-05 23:26:46 +0200

    tab bar
    migliorato la tab bar

--- SoTourist/Front-End/src/app/tabs/tabs.page.ts
+++ SoTourist/Front-End/src/app/tabs/tabs.page.ts
@@ -1,5 +1,6 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { Router, NavigationEnd } from '@angular/router';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square } from 'ionicons/icons';
 
@@ -7,12 +8,21 @@ import { triangle, ellipse, square } from 'ionicons/icons';
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
+  standalone: true,
   imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
+  public selectedTab: string = 'home';
 
-  constructor() {
+  constructor(private router: Router) {
     addIcons({ triangle, ellipse, square });
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const path = this.router.url.split('/')[2]; // /tabs/home → "home"
+        this.selectedTab = path;
+      }
+    });
   }
 }
