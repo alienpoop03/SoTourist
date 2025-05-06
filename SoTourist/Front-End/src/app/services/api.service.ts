024a8322a99f01bf624158b212d2f99cfdda4f90
Gabry-Fara <gabriele.faraone.gf@gmail.com>
2025-05-07 00:25:03 +0200

    itinerario

--- SoTourist/Front-End/src/app/services/api.service.ts
+++ SoTourist/Front-End/src/app/services/api.service.ts
@@ -10,14 +10,30 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
-  getItinerary(city: string, totalDays: number, accommodation?: string): Observable<{ itinerary: any[], coverPhoto: string }> {
-    const params = new HttpParams()
+  getItinerary(
+    city: string,
+    totalDays: number,
+    accommodation?: string,
+    extra?: { mustSee?: string[], transport?: string, aiQuestion?: string }
+  ): Observable<{ itinerary: any[], coverPhoto: string }> {
+    let params = new HttpParams()
       .set('city', city)
       .set('totalDays', totalDays.toString())
       .set('accommodation', accommodation || '');
-
+  
+    if (extra?.mustSee?.length) {
+      params = params.set('mustSee', extra.mustSee.join(','));
+    }
+    if (extra?.transport) {
+      params = params.set('transport', extra.transport);
+    }
+    if (extra?.aiQuestion) {
+      params = params.set('aiQuestion', extra.aiQuestion);
+    }
+  
     return this.http.get<{ itinerary: any[], coverPhoto: string }>(
       `${this.BASE_URL}/itinerary`, { params }
     );
   }
+  
 }
