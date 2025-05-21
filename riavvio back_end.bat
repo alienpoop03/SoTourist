a34e4931d9cdc7f74b59d206ee05638cbd354623
Salvuccio03 <salvatoreoteri03@gmail.com>
2025-05-21 09:41:20 +0200

    Create riavvio back_end.bat

---
+++ riavvio back_end.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Avvio deploy remoto su Render...
+curl -X POST https://api.render.com/deploy/srv-d0lflehr0fns738bstog?key=8kYC9udcKhQ
+echo Fatto. Premi un tasto per chiudere.
+pause
