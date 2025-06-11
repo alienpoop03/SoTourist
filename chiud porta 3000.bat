35fc6ed6fc756a69f50b950943a965963e4dafd9
Gabry-Fara <gabriele.faraone.gf@gmail.com>
2025-06-11 16:47:05 +0200

    eliminazione
    eliminazione file inutili

--- chiud porta 3000.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo 🔒 Rimozione regola firewall per Express porta 3000...
-netsh advfirewall firewall delete rule name="Express 3000"
-echo ✅ Porta 3000 bloccata nuovamente. Premi un tasto per uscire.
-pause
